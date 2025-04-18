f2d6c154201b7413213273eca685d4c4be927773
Jigar <jigarce007@gmail.com>
2025-04-18 22:35:13 +0800

    login user basic -- without alerts

--- router/veiwRouter.js
+++ router/veiwRouter.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const viewController = require('../controller/viewController');
+const authcontroller = require('../controller/authController');
 
 const router = express.Router();
+
+router.use(authcontroller.isLoggedIn);
 
 router.get('/', viewController.getAllTours);
 router.get('/tour/:slug', viewController.getTour);
