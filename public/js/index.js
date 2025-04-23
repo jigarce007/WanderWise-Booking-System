cf9b7da44016587701e0db394a984e0156e46ae9
Jigar <jigarce007@gmail.com>
2025-04-23 17:37:48 +0800

    Added package bundle Parcel and finished login functionality with alerts

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+import '@babel/polyfill';
+import { login } from './login.js';
+import { displayMap } from './mapbox.js';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiamlnYXJjZTAwNyIsImEiOiJjbTlqYWQyam4wOWtxMmpzZzdvcmtsMWVzIn0.A0egDu9561KGf-I1autMbA';
+
+const mapbox = document.getElementById('map');
+const loginform = document.querySelector('.form');
+if (mapbox) {
+  const locations = JSON.parse(mapbox.dataset.locations);
+  console.log(` Locations : ${JSON.stringify(locations)}`);
+  displayMap(locations);
+}
+
+if (loginform) {
+  loginform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
